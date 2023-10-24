7f041cd8a6d6fc7451ba324e410733dd7e85acd0
swastika <swastikasharma596@gmail.com>
2023-10-24 15:38:35 +0530

    Initial Commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://swastikasharma596:bRzw9f7D1TrDEht9@bookstore.cttbtax.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
